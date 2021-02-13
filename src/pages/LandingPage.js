d6b36c7d6b3f08dbbbb6a3892627eb95c977c7b8
Oktado Putra <oktado.putra@gmail.com>
2021-02-13 11:10:40 +0700

    create section mostpicked column

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+
+import landingPage from "Json/landingPage";
+
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.MostPicked} />
       </>
     );
   }
--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "components/Button";
+import Button from "components/Button/index";
 
 export default function IconText() {
   return (
